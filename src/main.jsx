a5310a45e2d0013d1781d9cb7b4b44a18e890b69
Adam Tarpey <tarpeyadam@gmail.com>
2023-11-11 23:08:09 -0500

    Starting point poke page

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,22 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+/*
+For this challenge you will create a pokemon finder/randomizer, where you will call the pokeAPI to get the data.
+For this app your user is requesting:
+        1. To be able to see some preselected pokemon on the main screen.
+        2. The user wants to be able to search for all pokemon via a search function.
+        3. Each pokemon should be able to be selected to view more details on said pokemon.
+        4. Within each pokemon details page the user wants to be able to save pokemon to favorites page.
+        Bonus:
+             The user wants to save the favorited pokemon to a local storage and if I have pokemon in my local storage I want them to be displayed to the front page thus when I 
+              refresh the page my favorite will be saved.
+*/
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
